16f1b93149da7751ad6276ca87ff38d63da70b3e
coder0144 <130371152+coder0144@users.noreply.github.com>
2025-04-06 15:47:07 +0530

    Add files via upload

---
+++ positive.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a;
+    cin>>a;
+
+    if(a>0){
+        cout<<"The number is positive:"<<endl;
+    }
+    else{
+        cout<<"the number is negative:"<<endl;
+    }
+    return 0;
+}
