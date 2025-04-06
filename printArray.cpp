fe5188149496463f22a1f90d9fab376876f1568a
coder0144 <130371152+coder0144@users.noreply.github.com>
2025-04-06 15:44:46 +0530

    Add files via upload

---
+++ printArray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int third[15]={2,7,4,7,9,5,6,7,8,9,1,23,24,25,26};
+int n=15;
+
+
+cout<<"printing the array"<<endl;
+
+for(int i =0;i<n;i++){
+   cout<<third[i]<<" ";
+}
+return 0;
+}
