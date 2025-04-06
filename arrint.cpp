a62a3d1d9ba8e1e8895409f755a753e060527e77
coder0144 <130371152+coder0144@users.noreply.github.com>
2025-04-06 15:46:17 +0530

    Add files via upload

---
+++ arrint.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[15] = {45,56};
+    int n = 15;
+
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+
+    }
+    return 0;
+}
